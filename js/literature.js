a4fcc78fb7cc7647e3f05aa4b29e52c9b758eb79
LEO 的MBA <macbook@macbookdeMacBook.local>
2026-04-14 16:54:32 -0100

    添加文学板块：翻译中国小说到Kriolu，包含6个故事

---
+++ js/literature.js
@@ -0,0 +1,373 @@
+// 中国文学故事数据 - 翻译为 Kriolu
+const storiesData = [
+    {
+        id: 1,
+        title: "O Ladrãu di Fogu",
+        originalTitle: "偷火者",
+        author: "Contu Popular",
+        genre: "folklore",
+        length: "short",
+        readTime: "8 minutu",
+        views: 1256,
+        likes: 89,
+        image: "https://images.unsplash.com/photo-1518709268805-4e9042af9f23?w=400",
+        summary: "Un kontu antigu di China sobri un omi ki rouba fogu di deus pa da pa humanidadi.",
+        content: `
+            <h2>O Ladrãu di Fogu</h2>
+            <p class="story-meta">Traduzidu di contu popular di China</p>
+            
+            <p>Na tempu antigu, humanu nunka tenha fogu. Es kumia karnin kru, i ta tremi na friu duranti tudu noti.</p>
+            
+            <p>Tinha un omi joven, nomi di Yi. Eli ta odja se povo sofri, i eli decide ki eli ten ki fazi alguma kusa.</p>
+            
+            <p>Yi subi montanha sagradu, undi deus vive. Eli ta sabi ki deus ten fogu, ma deus nunka kere parti ku humanu.</p>
+            
+            <p>"Pai Deus," Yi ta dizi, "pou favôr, da-nos fogu. Nos ta mori di friu."</p>
+            
+            <p>Deus ta ri. "Fogu é pa deus sô. Humanu ki fika na tera."</p>
+            
+            <p>Ma Yi nunka desisti. Na un noti skuru, eli entra na templo di deus i rouba un brassa di fogu sagradu.</p>
+            
+            <p>Deus ta diskubri i ta manda soldadu pa pega Yi. Ma Yi ta kore rapidu, ta guarda fogu na se korason.</p>
+            
+            <p>Yi volta pa se povo i ensina es komu uza fogu. Desdi enton, humanu ten fogu pa kuzinha, pa kenti, i pa ilumina noti.</p>
+            
+            <p>Ma Yi paga preçu. Deus ta kastiga eli, manda eli vive na montanha so. Ma Yi kontenti, saben ki eli salba se povo.</p>
+            
+            <p>Ate oji, na China, genti ta konta es kontu pa lembra ki un omi valienti da-luz pa mundu.</p>
+        `
+    },
+    {
+        id: 2,
+        title: "A Raposa i As Uva",
+        originalTitle: "狐狸与葡萄",
+        author: "Contu di Fabulas",
+        genre: "wisdom",
+        length: "short",
+        readTime: "5 minutu",
+        views: 2341,
+        likes: 156,
+        image: "https://images.unsplash.com/photo-1516594915697-87eb3b1c14ea?w=400",
+        summary: "Un fabula klasiku sobri raposa ki nunka konsigi uva i ta diz ki es amargu.",
+        content: `
+            <h2>A Raposa i As Uva</h2>
+            <p class="story-meta">Traduzidu di fabula di China</p>
+            
+            <p>Tinha un raposa ki ta pasia na boscaji. Eli ta odja un parra di uva, ta pendura altu na arvori.</p>
+            
+            <p>"Uva!" raposa ta pensa. "Es ta parse midju. Mi kere kumi es."</p>
+            
+            <p>Raposa ta salta. Ma es ta altu dimas. Eli ta salta otu be. Ma es ta altu ainda.</p>
+            
+            <p>Eli ta tenta otu be, i otu be. Ma kada be, es ta fika na mesmu lugar, altu, inalcançavel.</p>
+            
+            <p>Finalmenti, raposa ta para. Eli ta odja pa uva, ta balansa kabesa.</p>
+            
+            <p>"Na verdadi," eli ta dizi, "mi nunka kere es uva. Es ta amargu dimas. Es nunka ta sta maduru."</p>
+            
+            <p>I raposa ta bai, ta fingi ki eli nunka kere uva.</p>
+            
+            <p>Moral di kontu: As bez, kantu nos nunka konsigi alguma kusa, nos ta diz ki nos nunka kere es, pa eskonde nos desapontamentu.</p>
+        `
+    },
+    {
+        id: 3,
+        title: "O Pescador i Sereia",
+        originalTitle: "渔夫与美人鱼",
+        author: "Contu Popular di Costa",
+        genre: "romance",
+        length: "medium",
+        readTime: "15 minutu",
+        views: 3421,
+        likes: 267,
+        image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400",
+        summary: "Un pescador pobre salba un sereia i ganha fortuna, ma eli perdi kel ki mais eli ama.",
+        content: `
+            <h2>O Pescador i Sereia</h2>
+            <p class="story-meta">Traduzidu di contu popular di costa di China</p>
+            
+            <p>Na un vilaju di peskador, tinha un omi joven nomi di Lin. Eli ta peskia kada dia, ma eli ta katcha poku peixi. Eli ta vive na pobreza.</p>
+            
+            <p>Un dia, kantu Lin ta lansa se rede, eli ta sinti alguma kusa pesadu. Eli ta puxa ku forsa, i na rede tinha un sereia!</p>
+            
+            <p>Sereia tinha skama verdhi, i kabellu longu pretu. Ela ta odja Lin ku oju grandi, ta tremi.</p>
+            
+            <p>"Pou favôr," sereia ta dizi, "larga-mi. Mi ta promete da-bu un deseju."</p>
+            
+            <p>Lin ta pensa. Eli pode pedi ouro. Eli pode pedi poder. Ma eli ta odja sereia, i eli ta sinti pena.</p>
+            
+            <p>"Mi larga-bu," Lin ta dizi. "Ma mi nka kere nada. Sô ki bu vive."</p>
+            
+            <p>Sereia ta sorri, un sorrisu luminosu. "Bu é bon. Bu merece rekonpensa."</p>
+            
+            <p>Ela ta da Lin un perola grandi. "Es ta da sorti. Kada be ki bu peskia, bu ta katcha peixi abundanti."</p>
+            
+            <p>I sereia ta nada pa mar, ta olha pa Lin otu be antes di desaparese.</p>
+            
+            <p>Desdi enton, Lin ta torna riku. Eli ta katcha peixi ki nunka eli ta odja. Eli ta konstrui kaza grandi. Genti ta respeita eli.</p>
+            
+            <p>Ma kada noti, Lin ta senta na praia, ta odja pa mar. Eli ta lembra sereia, i eli ta sinti un vaziu na korason.</p>
+            
+            <p>"Mi ten tudu," eli ta pensa, "ma mi nunka ta odja ela otu be."</p>
+            
+            <p>Un dia, kantu Lin sta velhu, eli ta volta pa mar. Eli ta entra na agua, ta nada pa profundidadi. Genti ta diz ki es ta odja un sereia ku kabellu pretu, ta abraça Lin, i es desaparese juntu.</p>
+            
+            <p>Ate oji, peskador na kel vilaju ta konta es kontu, i es ta kre ki sereia i peskador ta vive feliz na fondo di mar.</p>
+        `
+    },
+    {
+        id: 4,
+        title: "O Sabi di Tres Pernas",
+        originalTitle: "三足乌",
+        author: "Mitu Antigu",
+        genre: "folklore",
+        length: "medium",
+        readTime: "12 minutu",
+        views: 1890,
+        likes: 134,
+        image: "https://images.unsplash.com/photo-1444464666168-49d633b86797?w=400",
+        summary: "Mitu sobri sabi di tres pernas ki ta leba sol, i por ki ten sol i lua.",
+        content: `
+            <h2>O Sabi di Tres Pernas</h2>
+            <p class="story-meta">Traduzidu di mitu antigu di China</p>
+            
+            <p>Na tempu di inisiu, tinha dius sabi di tres pernas. Es vive na un arvori sagradu, i kada dia un sabi ta leba sol pa viaja pa selu.</p>
+            
+            <p>Un sabi ta levanta na manha, ta karga sol nascenti. Eli ta voa pa lesti, ta leba luz i kenti pa mundu.</p>
+            
+            <p>Ma un dia, dius sabi ta levanta juntu. Es kre ki es ta leba sol otu be. Es ta briga, i es ta leba sol juntu pa selu.</p>
+            
+            <p>Agora tinha trez sol na selu! Tera ta kema. Riu ta sekadu. Planta ta mori. Humanu ta sofri.</p>
+            
+            <p>Un arqueiru valienti, nomi Hou Yi, ta subi montanha altu. Eli ten arku i seta magicu. Eli ta dispara seta, i un sabi ta kai.</p>
+            
+            <p>Eli ta dispara otu be, i otu sabi ta kai. Ma kantu eli ta dispara oitavu seta, un omi velhu ta aparese.</p>
+            
+            <p>"Para!" omi velhu ta grita. "Se bu mata oitavu sabi, mundu ta fika na skuru pa senpri!"</p>
+            
+            <p>Hou Yi ta para. Eli ta odja pa selu. Agora sô tinha un sol, i eli ta brilha bon.</p>
+            
+            <p>Ma sabi ki Hou Yi mata? Es ta transforma na lua. Na noiti, kantu sol ta bai, lua ta aparese, ta lembra genti di sabi ki ta mori pa salba mundu.</p>
+            
+            <p>I asina, nos ten dia i noiti, sol i lua, i equilibriu na universu.</p>
+        `
+    },
+    {
+        id: 5,
+        title: "O Sabi i a Kornija",
+        originalTitle: "乌鸦喝水",
+        author: "Contu di Fabulas",
+        genre: "wisdom",
+        length: "short",
+        readTime: "5 minutu",
+        views: 4567,
+        likes: 389,
+        image: "https://images.unsplash.com/photo-1552728089-57bdde30beb3?w=400",
+        summary: "Un sabi sedentu ta atxa un manera inteligenti di bebi agua.",
+        content: `
+            <h2>O Sabi i a Kornija</h2>
+            <p class="story-meta">Traduzidu di fabula di China</p>
+            
+            <p>Un dia kaler, un sabi ta voa na selu. Eli ta buska agua, ma eli nunka atxa.</p>
+            
+            <p>Finalmenti, eli ta odja un kornija na tera. Eli ta bai pri, ma kantu eli ta olha para dentu, agua sta na fundu. Eli nunka konsigi bebi.</p>
+            
+            <p>Sabi ta pensa. Eli ta tenta derruba kornija, ma es pesadu. Eli ta tenta parti kornija, ma es fort.</p>
+            
+            <p>Enton sabi ta odja pedra na tera. Un ideia ta ben na se kabesa.</p>
+            
+            <p>Eli ta pega un pedra ku se boku i ta larga na kornija. Plim! Agua ta subi un poku.</p>
+            
+            <p>Sabi ta sorri. Eli ta kore, ta pega mais pedra. Un, doi, trez... eli ta konta.</p>
+            
+            <p>Kada pedra ki eli ta larga, agua ta subi mais. Ate ki... sim! Agua ta xiga na boku di kornija!</p>
+            
+            <p>Sabi ta bebi agua, ta senti refresku na garganta. Eli ta kontenti.</p>
+            
+            <p>Moral di kontu: Kantu bu ten problema, pensa. Inteligensi ta vense forsa.</p>
+        `
+    },
+    {
+        id: 6,
+        title: "A Princesa i O Pintor",
+        originalTitle: "画中人",
+        author: "Contu Romantiku",
+        genre: "romance",
+        length: "long",
+        readTime: "25 minutu",
+        views: 2134,
+        likes: 198,
+        image: "https://images.unsplash.com/photo-1578301978693-85fa9c0320b9?w=400",
+        summary: "Un pintor ta kria pintura di princesa ki ta ben na vida, ma amor es ta proibidu.",
+        content: `
+            <h2>A Princesa i O Pintor</h2>
+            <p class="story-meta">Traduzidu di contu romantiku di China</p>
+            
+            <p>Na dinastia Tang, tinha un pintor joven nomi di Zhang. Eli ta vive na pekinu, i eli ta pinta pa sobrevive.</p>
+            
+            <p>Un dia, eli ta odja un princesa ki ta pasa na rua. Ela ta beleza dimas, ku pele di sidu i oju ki ta brilha komu estrela.</p>
+            
+            <p>Zhang ta keda katuadu. Eli nunka ta odja algen ki ta beleza asina. Ma eli sabi ki eli é pobre, i princesa nunka ta odja eli.</p>
+            
+            <p>Enton eli ta decide pinta ela. Eli ta trabaja durante noti inteiru, ta kria pintura ki ta mostra beleza di princesa.</p>
+            
+            <p>Kantu eli ta termina, eli ta odja pintura, i eli ta suspira. "Se sô bu podia ben na vida," eli ta dizi.</p>
+            
+            <p>Enton, kusa stranju ta acontece. Pintura ta brilha, i princesa ta sai fora di pintura!</p>
+            
+            <p>"Bu ta kama-mi?" ela ta perguntia, ta sorri.</p>
+            
+            <p>Zhang nunka ta kre ki es ta verdadi. Ma ela sta li, na se kaza, ta respira, ta vive.</p>
+            
+            <p>"Mi sta na pintura durante anu," ela ta explica. "Mi ta odja mundu atraves di oju di bu. Bu amor ta da-mi vida."</p>
+            
+            <p>Zhang i princesa ta vive juntu durante dias. Es ta ri, es ta kanta, es ta ama. Ma es ten ki eskonde, pasá princesa nunka pode sai pa fora.</p>
+            
+            <p>Ma notisia ta kore. Imperador ta sabi ki princesa ta desaparese, i eli ta manda soldadu buska ela.</p>
+            
+            <p>Un dia, soldadu ta bate na porta di Zhang. "Nos sabi ki princesa sta li!" es ta grita.</p>
+            
+            <p>Zhang ta odja pa princesa. Ela ta tristi, ma ela ta sabi ki es ten ki separa.</p>
+            
+            <p>"Volta pa pintura," Zhang ta dizi, voz ta tremi. "Li, bu ta sta siguru."</p>
+            
+            <p>Princesa ta beija Zhang, i ela ta volta pa pintura. Kantu soldadu ta entra, es nunka atxa nada.</p>
+            
+            <p>Zhang ta guarda pintura na lugar siguru. Kada dia, eli ta odja pa ela, i eli ta sabi ki ela sta li, ta vive na mundu di pintura, ta espera eli.</p>
+            
+            <p>Kantu Zhang ta mori, eli ta deixa pintura pa se filhu, ku instrusaun: "Nunka venda es. Es é amor di mi vida."</p>
+            
+            <p>Ate oji, pintura ta sta na museu, i genti ta diz ki as vezi, na noiti di lua cheiu, bu pode odja princesa ta sorri na pintura.</p>
+        `
+    }
+];
+
+// 渲染故事卡片
+function renderStories(stories) {
+    const container = document.getElementById('stories-container');
+    if (!container) return;
+    
+    if (stories.length === 0) {
+        container.innerHTML = `
+            <div class="no-results-modern">
+                <i class="fas fa-book"></i>
+                <p>Nada atxa ku es filtru</p>
+                <button onclick="clearFilters()" class="btn-primary-modern">Limpa filtru</button>
+            </div>
+        `;
+        return;
+    }
+    
+    container.innerHTML = stories.map(story => `
+        <article class="story-card-modern" onclick="viewStory(${story.id})">
+            <div class="story-image-modern">
+                <img src="${story.image}" alt="${story.title}" loading="lazy">
+                <span class="story-genre-badge">${getGenreLabel(story.genre)}</span>
+            </div>
+            <div class="story-content-modern">
+                <h3 class="story-title-modern">${story.title}</h3>
+                <p class="story-original-title">${story.originalTitle}</p>
+                <p class="story-author-modern"><i class="fas fa-user-edit"></i> ${story.author}</p>
+                <p class="story-summary-modern">${story.summary}</p>
+                <div class="story-meta-modern">
+                    <span><i class="fas fa-clock"></i> ${story.readTime}</span>
+                    <span><i class="fas fa-eye"></i> ${formatNumber(story.views)}</span>
+                    <span><i class="fas fa-heart"></i> ${formatNumber(story.likes)}</span>
+                </div>
+            </div>
+        </article>
+    `).join('');
+    
+    // 更新计数
+    const countEl = document.getElementById('results-count');
+    if (countEl) {
+        countEl.textContent = stories.length + ' kontu';
+    }
+}
+
+// 获取分类标签
+function getGenreLabel(genre) {
+    const labels = {
+        'humor': 'Humor',
+        'romance': 'Romansi',
+        'adventure': 'Aventura',
+        'wisdom': 'Sabedoria',
+        'folklore': 'Folklore'
+    };
+    return labels[genre] || genre;
+}
+
+// 格式化数字
+function formatNumber(num) {
+    if (num >= 1000) {
+        return (num / 1000).toFixed(1) + 'k';
+    }
+    return num;
+}
+
+// 查看故事详情
+function viewStory(id) {
+    const story = storiesData.find(s => s.id === id);
+    if (!story) return;
+    
+    // 存储当前故事到 sessionStorage
+    sessionStorage.setItem('currentStory', JSON.stringify(story));
+    
+    // 跳转到详情页
+    window.location.href = `story-detail.html?id=${id}`;
+}
+
+// 应用筛选
+function applyFilters() {
+    const genre = document.getElementById('filter-genre')?.value;
+    const length = document.getElementById('filter-length')?.value;
+    
+    let filtered = storiesData;
+    
+    if (genre) {
+        filtered = filtered.filter(s => s.genre === genre);
+    }
+    
+    if (length) {
+        filtered = filtered.filter(s => s.length === length);
+    }
+    
+    renderStories(filtered);
+}
+
+// 清除筛选
+function clearFilters() {
+    const genreSelect = document.getElementById('filter-genre');
+    const lengthSelect = document.getElementById('filter-length');
+    
+    if (genreSelect) genreSelect.value = '';
+    if (lengthSelect) lengthSelect.value = '';
+    
+    renderStories(storiesData);
+}
+
+// 渲染热门故事
+function renderPopularStories() {
+    const container = document.getElementById('popular-stories');
+    if (!container) return;
+    
+    const popular = [...storiesData]
+        .sort((a, b) => b.views - a.views)
+        .slice(0, 3);
+    
+    container.innerHTML = popular.map((story, index) => `
+        <div class="popular-story-item" onclick="viewStory(${story.id})">
+            <span class="popular-rank">${index + 1}</span>
+            <div class="popular-info">
+                <h5>${story.title}</h5>
+                <span>${formatNumber(story.views)} leitura</span>
+            </div>
+        </div>
+    `).join('');
+}
+
+// 初始化
+document.addEventListener('DOMContentLoaded', () => {
+    renderStories(storiesData);
+    renderPopularStories();
+});
